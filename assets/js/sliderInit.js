0738ab6e138c6f15feb5848e7b8812daf1e05094
Mayowa Falade <mayowa.falade@ithaka.org>
2016-10-12 18:20:01 +0100

    A bunch of fixes:
    + Add new slider enginer.
    + Address hero section display on mobile
    + General touches

--- assets/js/sliderInit.js
+++ assets/js/sliderInit.js
@@ -1,9 +1,14 @@
-$(document).on('ready', function() {
-		 $(".center").slick({
-			 dots: false,
-			 infinite: true,
-			 centerMode: true,
-			 slidesToShow: 3,
-			 slidesToScroll: 3
-		 });
-	 });
+$(document).ready(function() {
+ 
+	$("#portfolioSlider").owlCarousel({
+		items : 4,
+    autoPlay : true,
+		stopOnHover: true,
+		autoPlay: 2500,
+    navigation : false,
+    navigationText : ["yay","nay"],
+    rewindNav : true,
+    scrollPerPage : false,
+	});
+ 
+});
