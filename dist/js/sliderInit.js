0280019aa0567a6a8c29a5f86e60887721f75b3a
Mayowa Falade <mayowa.falade@ithaka.org>
2016-10-13 03:07:45 +0100

    Improve project structure

--- dist/js/sliderInit.js
+++ dist/js/sliderInit.js
@@ -1,14 +1,13 @@
-$(document).ready(function() {
- 
-	$("#portfolioSlider").owlCarousel({
-		items : 4,
-    autoPlay : true,
-		stopOnHover: true,
-		autoPlay: 2500,
-    navigation : false,
-    navigationText : ["yay","nay"],
-    rewindNav : true,
-    scrollPerPage : false,
-	});
- 
+"use strict";
+
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
+$(document).ready(function () {
+  var _$$owlCarousel;
+
+  $("#portfolioSlider").owlCarousel((_$$owlCarousel = {
+    items: 4,
+    autoPlay: true,
+    stopOnHover: true
+  }, _defineProperty(_$$owlCarousel, "autoPlay", 2500), _defineProperty(_$$owlCarousel, "navigation", false), _defineProperty(_$$owlCarousel, "navigationText", ["yay", "nay"]), _defineProperty(_$$owlCarousel, "rewindNav", true), _defineProperty(_$$owlCarousel, "scrollPerPage", false), _$$owlCarousel));
 });
---
+++ src/scripts/sliderInit.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+ 
+	$("#portfolioSlider").owlCarousel({
+		items : 4,
+    autoPlay : true,
+		stopOnHover: true,
+		autoPlay: 2500,
+    navigation : false,
+    navigationText : ["yay","nay"],
+    rewindNav : true,
+    scrollPerPage : false,
+	});
+ 
+});
