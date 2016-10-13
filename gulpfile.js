91badf43d291de48a0523348c08e4b7e9cd1df62
Mayowa Falade <mayowa.falade@ithaka.org>
2016-10-13 02:55:11 +0100

    Touches
    Add more meta tags for search engine indexing
    
    Move all images to AWS
    
    Move all static assets to dist folder

--- gulpfile.js
+++ gulpfile.js
@@ -4,23 +4,26 @@ var gulp = require('gulp'),
     path = require('path'),
     jade = require('gulp-jade'),
     sass = require('gulp-sass'),
+    babel = require('gulp-babel'),
     browserSync = require('browser-sync').create();
 
 var source_files = {
     sass: './src/sass/**/*.sass',
-    jade: './src/jade/**/*.jade'
+    jade: './src/jade/**/*.jade',
+    scripts: './src/scripts/*.js'
 };
 
 var output_files = {
     html: './',
-    css: './assets/css/'
+    css: './dist/css/',
+    js: './dist/js/build.js'
 };
 
 
 gulp.task('jade', function () {
     var locals = {};
     gulp.src(source_files.jade)
-        .pipe(jade({locals: locals}))
+        .pipe(jade({ locals: locals }))
         .pipe(gulp.dest(output_files.html))
         .pipe(browserSync.stream());
 });
@@ -34,7 +37,16 @@ gulp.task('sass', function () {
 });
 
 
-gulp.task('serve', ['sass', 'jade'], function() {
+gulp.task('babelify', function () {
+    return gulp.src(source_files.scripts)
+        .pipe(babel({
+            presets: ['es2015']
+        }))
+        .pipe(gulp.dest('dist/js'));
+});
+
+
+gulp.task('serve', ['sass', 'jade', 'babelify'], function () {
 
     browserSync.init({
         server: "./"
@@ -42,6 +54,7 @@ gulp.task('serve', ['sass', 'jade'], function() {
 
     gulp.watch(source_files.jade, ['jade']);
     gulp.watch(source_files.sass, ['sass']);
+    gulp.watch(source_files.scripts, ['babelify']);
     gulp.watch('./index.html').on('change', browserSync.reload);
 });
 
